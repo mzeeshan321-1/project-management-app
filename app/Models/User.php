370e5d157cc1742580ca80a0378780abf6dbc73f
zeeshan <mzeeshan321-1>
2025-07-15 11:59:30 +0500

    Second commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    // use HasRoles;
+
 
     /**
      * The attributes that are mass assignable.
---
+++ resources/views/superAdmin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('superAdmin.layouts.app')
+
+@section('title')
+    <title>Dashboard - Super Admin</title>
+
+@section('content')
+    <div class="pagetitle">
+        <h1>Dashboard</h1>
+    </div><!-- End Page Title -->
+@endsection
